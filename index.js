010eaaec893ab74c499c652daffe66224ab7643f
Erick <estudodoerick@gmail.com>
2023-01-31 17:29:54 -0300

    rotas_1.1_utilizando_core_modules

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
+const http = require('http');
 const fs = require('fs');
+const url = require('url');
 
-fs.rename('arquivo.txt', 'arquivoNovo.txt', function (error) {
-  if (error) {
-    console.log(error);
-    return;
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  const q = url.parse(req.url, true);
+  const filename = q.pathname.substring(1);
+
+  if (filename.includes('html')) {
+    if (fs.existsSync(filename)) {
+      fs.readFile(filename, function (error, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+      });
+    } else {
+      fs.readFile('error.html', function (error, data) {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+      });
+    }
   }
-  console.log('Nome alterado com sucesso!');
 });
+server.listen(port, () => {
+  console.log(`Servidor iniciado na porta ${port}`);
+});
+
+// para ir para a página tem que acessaar localhos:3000/index.html
