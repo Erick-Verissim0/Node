971b8a5579974d2a9c2980723de4a089614c84b7
Erick <estudodoerick@gmail.com>
2023-01-31 00:56:39 -0300

    http_1.1

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const EventEmitter = require('events');
-const eventEmitter = new EventEmitter();
+const http = require('http');
+const port = 3000;
 
-eventEmitter.on('start', () => {
-  console.log('Durante');
+const server = http.createServer((req, res) => {
+  res.write('Oi, HTTP!');
+  res.end(); // tem que sinalizar quando para o response
 });
 
-console.log('Antes');
-
-eventEmitter.emit('start');
-
-console.log('Depois');
+server.listen(port, () => {
+  console.log(`Servidor rodando na porta: ${port}`);
+});
