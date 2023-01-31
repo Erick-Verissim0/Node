56a299fbd06cb4cb4d7432b65f9ededfd0253c11
Erick <estudodoerick@gmail.com>
2023-01-31 15:38:30 -0300

    fs_1.4_removendo_arquivo_unlink

--- index.js
+++ index.js
@@ -1,30 +1,11 @@
-const http = require('http');
 const fs = require('fs');
 
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  const urlInfo = require('url').parse(req.url, true);
-  const name = urlInfo.query.name;
-
-  if (!name) {
-    fs.readFile('index.html', function (err, data) {
-      res.writeHead(200, { 'Content-Type': 'text-html' });
-      res.write(data);
-      return res.end();
-    });
-  } else {
-    const nameNewFile = name + '\r\n';
-
-    fs.appendFile('arquivo.txt', nameNewFile, function (err, data) {
-      res.writeHead(302, {
-        Location: '/',
-      });
-      return res.end();
-    });
+fs.unlink('arquivo.txt', function (error) {
+  if (error) {
+    console.log(error);
+    return;
   }
+  console.log('Erro removido com sucesso!');
 });
 
-server.listen(port, () => {
-  console.log(`Servidor rodando na porta: ${port}`);
-});
+// se der tudo certo o arquivo com o nome "arquivo.txt" vai ser deletado
